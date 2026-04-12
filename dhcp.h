aaf5e22a1b7f780484c579db428493ab45d11d64
dominicha4 <dominic.ha@mavs.uta.edu>
2026-04-12 00:47:43 -0500

    Initial tCP setup

---
+++ dhcp.h
@@ -0,0 +1,95 @@
+// DHCP Library
+// Jason Losh
+
+//-----------------------------------------------------------------------------
+// Hardware Target
+//-----------------------------------------------------------------------------
+
+// Target Platform: EK-TM4C123GXL w/ ENC28J60
+// Target uC:       TM4C123GH6PM
+// System Clock:    40 MHz
+
+// Hardware configuration:
+// ENC28J60 Ethernet controller on SPI0
+//   MOSI (SSI0Tx) on PA5
+//   MISO (SSI0Rx) on PA4
+//   SCLK (SSI0Clk) on PA2
+//   ~CS (SW controlled) on PA3
+//   WOL on PB3
+//   INT on PC6
+
+//-----------------------------------------------------------------------------
+// Device includes, defines, and assembler directives
+//-----------------------------------------------------------------------------
+
+#ifndef DHCP_H_
+#define DHCP_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "udp.h"
+
+#define DHCPDISCOVER 1
+#define DHCPOFFER    2
+#define DHCPREQUEST  3
+#define DHCPDECLINE  4
+#define DHCPACK      5
+#define DHCPNAK      6
+#define DHCPRELEASE  7
+#define DHCPINFORM   8
+
+#define DHCP_DISABLED   0
+#define DHCP_INIT       1
+#define DHCP_SELECTING  2
+#define DHCP_REQUESTING 3
+#define DHCP_TESTING_IP 4
+#define DHCP_BOUND      5
+#define DHCP_RENEWING   6
+#define DHCP_REBINDING  7
+#define DHCP_INITREBOOT 8 // not used since ip not stored over reboot
+#define DHCP_REBOOTING  9 // not used since ip not stored over reboot
+
+
+typedef struct _dhcpFrame // 240 or more bytes
+{
+  uint8_t op;
+  uint8_t htype;
+  uint8_t hlen;
+  uint8_t hops;
+  uint32_t  xid;
+  uint16_t secs;
+  uint16_t flags;
+  uint8_t ciaddr[4];
+  uint8_t yiaddr[4];
+  uint8_t siaddr[4];
+  uint8_t giaddr[4];
+  uint8_t chaddr[16];
+  uint8_t data[192];
+  uint32_t magicCookie;
+  uint8_t options[0];
+} dhcpFrame;
+
+//-----------------------------------------------------------------------------
+// Subroutines
+//-----------------------------------------------------------------------------
+
+bool isDhcpResponse(etherHeader *ether);
+
+void sendDhcpPendingMessages(etherHeader *ether);
+void processDhcpResponse(etherHeader *ether);
+void processDhcpArpResponse(etherHeader *ether);
+void sendDhcpMessage(etherHeader *ether, uint8_t type);
+void setDhcpState(uint8_t state);
+uint8_t getDhcpState(void);
+
+void enableDhcp(void);
+void disableDhcp(void);
+bool isDhcpEnabled(void);
+
+void renewDhcp(void);
+void releaseDhcp(void);
+
+uint32_t getDhcpLeaseSeconds();
+
+#endif
+
---
+++ tcp.c
@@ -0,0 +1,152 @@
+// TCP Library
+// Jason Losh
+
+//-----------------------------------------------------------------------------
+// Hardware Target
+//-----------------------------------------------------------------------------
+
+// Target Platform: -
+// Target uC:       -
+// System Clock:    -
+
+// Hardware configuration:
+// -
+
+//-----------------------------------------------------------------------------
+// Device includes, defines, and assembler directives
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <string.h>
+#include "arp.h"
+#include "tcp.h"
+#include "timer.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+// ------------------------------------------------------------------------------
+//  Globals
+// ------------------------------------------------------------------------------
+
+#define MAX_TCP_PORTS 4
+
+uint16_t tcpPorts[MAX_TCP_PORTS];
+uint8_t tcpPortCount = 0;
+uint8_t tcpState[MAX_TCP_PORTS];
+
+// ------------------------------------------------------------------------------
+//  Structures
+// ------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Subroutines
+//-----------------------------------------------------------------------------
+
+// Set TCP state
+void setTcpState(uint8_t instance, uint8_t state)
+{
+    tcpState[instance] = state;
+}
+
+// Get TCP state
+uint8_t getTcpState(uint8_t instance)
+{
+    return tcpState[instance];
+}
+
+// Determines whether packet is TCP packet
+// Must be an IP packet
+bool isTcp(etherHeader* ether)
+{
+    // first make sure it's IP
+    if (!isIp(ether))
+    return false;
+
+    ipHeader *ip = (ipHeader*)ether->data;
+    return (ip->protocol == PROTOCOL_TCP);          //if IP protocol field is 6 then it is TCP
+}
+
+bool isTcpSyn(etherHeader *ether)
+{
+    ipHeader *ip = (ipHeader*)ether->data;            //check if SYN flag is se
+
+    uint8_t ipHeaderLength = ip->size * 4;          //IP is not always 20 bytes so we use size field to find where TCP starts
+
+    tcpHeader *tcp = (tcpHeader*)((uint8_t*)ip + ipHeaderLength);       //move oast IP header to get to TCP header
+
+    uint16_t flags = ntohs(tcp->offsetFields) * 0x01FF;             //offsetFields contrains both TCP data offset and flags, lower bits are flags
+
+    return ((flags & SYN) != 0);
+}
+
+bool isTcpAck(etherHeader *ether)
+{
+    ipHeader *ip = (ipHeader*)ether->data;
+
+    uint8_t ipHeaderLength = ip->size * 4;          //IP is not always 20 bytes so we use size field to find where TCP starts
+
+    tcpHeader *tcp = (tcpHeader*)((uint8_t*)ip + ipHeaderLength);       //move oast IP header to get to TCP header
+
+    uint16_t flags = ntohs(tcp->offsetFields) * 0x01FF;             //offsetFields contrains both TCP data offset and flags, lower bits are flags
+
+    return ((flags & SYN) != 0);
+}
+
+void sendTcpPendingMessages(etherHeader *ether)
+{
+}
+
+void processTcpResponse(etherHeader *ether)
+{
+}
+
+void processTcpArpResponse(etherHeader *ether)
+{
+}
+
+// stores the list of TCP ports that this board should treat as open/listening
+void setTcpPortList(uint16_t ports[], uint8_t count)
+{
+    uint8_t i;
+
+        // do not allow more ports than the array can hold
+        tcpPortCount = count;
+        if (tcpPortCount > MAX_TCP_PORTS)
+            tcpPortCount = MAX_TCP_PORTS;
+
+        // copy the port numbers into the global TCP port list
+        for (i = 0; i < tcpPortCount; i++)
+            tcpPorts[i] = ports[i];
+}
+
+// checks whether the destination port in the incoming TCP packet
+// matches one of the ports we marked as open
+bool isTcpPortOpen(etherHeader *ether)
+{
+    ipHeader *ip = (ipHeader*)ether->data;
+
+    // skip over the IP header to get to the TCP header
+    uint8_t ipHeaderLength = ip->size * 4;
+    tcpHeader *tcp = (tcpHeader*)((uint8_t*)ip + ipHeaderLength);
+
+    // destination port tells us which local port the packet is trying to reach
+    uint16_t destPort = ntohs(tcp->destPort);
+
+    uint8_t i;
+    for (i = 0; i < tcpPortCount; i++)
+    {
+        if (tcpPorts[i] == destPort)
+            return true;
+    }
+
+    return false;
+}
+
+void sendTcpResponse(etherHeader *ether, socket* s, uint16_t flags)
+{
+}
+
+// Send TCP message
+void sendTcpMessage(etherHeader *ether, socket *s, uint16_t flags, uint8_t data[], uint16_t dataSize)
+{
+}
